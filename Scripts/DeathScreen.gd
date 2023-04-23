85736b52676922b419f28929b99ef611d24f55b6
8by4 <aidanquinn147@gmail.com>
2023-04-23 15:41:59 -0400

    Buttons.

---
+++ Scripts/DeathScreen.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+func _input(event):
+	if Input.is_action_just_pressed("interact"):
+		find_and_use_dialogue()
+
+func find_and_use_dialogue():
+	var dialogue_player = get_node_or_null("EndDialogue")
+	
+	if dialogue_player:
+		dialogue_player.play()
---
+++ Scripts/EndDialogue.gd
@@ -0,0 +1,49 @@
+extends CanvasLayer
+
+export(String, FILE, "*json") var dialogue_file
+
+var dialogues = []
+var currentDialogue = -1
+var isDialogueActive = false
+
+func _ready():
+	$NinePatchRect.visible = false
+
+func play():
+	if isDialogueActive:
+		return
+	
+	dialogues = load_dialogues()
+	
+	isDialogueActive = true
+	$NinePatchRect.visible = true
+	#currentDialogue = -1
+	next_line()
+
+func _input(event):
+	if not isDialogueActive:
+		return
+	if event.is_action_pressed("interact"):
+		next_line()
+
+
+func next_line():
+	currentDialogue += 1
+	if currentDialogue >= len(dialogues):
+		currentDialogue = -1
+		$Timer.start()
+		$NinePatchRect.visible = false
+		
+		return
+	
+	$NinePatchRect/Message.text = dialogues[currentDialogue]["text"]
+
+func load_dialogues():
+	var file = File.new()
+	if file.file_exists(dialogue_file):
+		file.open(dialogue_file, file.READ)
+		return parse_json(file.get_as_text())
+
+
+func _on_Timer_timeout():
+	isDialogueActive = false
